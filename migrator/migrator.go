4bf03d4867757bc3bba9d5c3fcd53ed0f0b11c01
s4bb4t <s4bb4t@yandex.ru>
2025-03-20 14:09:00 +0300

    Refactor migrator to simplify database connection handling.
    Replaced the custom Connector interface with the standard *sql.DB type for database connection in Migrate and Downgrade functions. This simplifies the code and aligns with common patterns, improving maintainability and reducing unnecessary abstraction.

--- migrator/migrator.go
+++ migrator/migrator.go
@@ -6,20 +6,16 @@ import (
 	"github.com/pressly/goose/v3"
 )
 
-type Connector interface {
-	DB() *sql.DB
-}
-
 // Migrate performs database migrations using the specified connection, table name, and SQL dialect.
 // Migrate use dir named `migrations` in root of project
-func Migrate(conn Connector, tableName, dialect string) error {
+func Migrate(conn *sql.DB, tableName, dialect string) error {
 	goose.SetTableName(fmt.Sprintf("public.%s", tableName))
 
 	if err := goose.SetDialect(dialect); err != nil {
 		return fmt.Errorf("dialect error: %w", err)
 	}
 
-	if err := goose.Up(conn.DB(), "migrations"); err != nil {
+	if err := goose.Up(conn, "migrations"); err != nil {
 		return fmt.Errorf("migrate error: %w", err)
 	}
 
@@ -28,14 +24,14 @@ func Migrate(conn Connector, tableName, dialect string) error {
 
 // Downgrade rolls back the latest database migration for the specified table using the provided connector and dialect.
 // Downgrade use dir named `migrations` in root of project
-func Downgrade(conn Connector, tableName, dialect string) error {
+func Downgrade(conn *sql.DB, tableName, dialect string) error {
 	goose.SetTableName(fmt.Sprintf("public.%s", tableName))
 
 	if err := goose.SetDialect(dialect); err != nil {
 		return fmt.Errorf("dialect error: %w", err)
 	}
 
-	if err := goose.Down(conn.DB(), "migrations/"); err != nil {
+	if err := goose.Down(conn, "migrations/"); err != nil {
 		return fmt.Errorf("migrate error: %w", err)
 	}
 
